b009522b13b83f2ff35d679736de335fec4cc546
Alexandre Angleraud <alexandre.angleraud@tut.fi>
2018-09-24 12:45:02 +0200

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../franka_tut_reasoning/src/franka_tut_reasoning'))
 
 # -- General configuration ------------------------------------------------
 
