f5c6ca54addf48df8f7210a2a2cc11b959515e57
Alexandre Angleraud <alexandre.angleraud@tut.fi>
2018-09-24 14:50:09 +0200

    update docs

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+#! /usr/bin/env python
+
+descr = "Teaching semantics and skills for human robot collaboration"
+
+from distutils.core import setup
+
+setup(
+    name='cogrob-tut-hri',
+    version='1.0',
+    maintainer='Alexandre Angleraud',
+    maintainer_email='alexandre.angleraud@tut.fi',
+    packages=['franka_tut_reasoning'],
+    license='LICENSE',
+    description= descr,
+    long_description=open('README.md').read(),
+)
