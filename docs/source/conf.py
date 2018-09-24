315fd2cd2f66172e4bc0b01ee6c594dc7c7b0a33
Alexandre Angleraud <alexandre.angleraud@tut.fi>
2018-09-24 08:11:36 +0200

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,20 +16,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sys
-import os
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+#import os
+#import sys
 #sys.path.insert(0, os.path.abspath('.'))
-
-# hack for readthedocs to cause it to run doxygen first
-# https://github.com/rtfd/readthedocs.org/issues/388
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-  from subprocess import call
-  call('doxygen')
 
 
 # -- General configuration ------------------------------------------------
--- franka_tut_reasoning/setup.py
+++ franka_tut_reasoning/setup.py
@@ -3,7 +3,10 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['franka_tut_reasoning'],
-    package_dir={'': 'src'}
-)
+    package_dir={'': 'src'},
+    install_requires=[
+          'breathe',
+      ]
+    )
 
 setup(**d)
